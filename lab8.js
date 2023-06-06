b79e9ae3b7c2138ffb86a13077500fca06a1fbc4
Елизавета Якунина <livya2003@mail.ru>
2023-06-06 13:15:33 +0700

    Вывод даты

---
+++ lab8.js
@@ -0,0 +1,25 @@
+function showDate() {
+    let out = document.getElementById('current-date');
+    let today = new Date();
+    out.innerHTML = 'Дата и время для русской локали: ' + today.toLocaleString('ru-RU');
+
+    let out1 = document.getElementById('current-date1');
+    let today1 = new Date();
+    out1.innerHTML = 'Дата и время для локали ОАЭ: ' + today1.toLocaleString('ar-AE');
+
+    let out2 = document.getElementById('current-date2');
+    let today2 = new Date();
+    out2.innerHTML = 'Дата и время для локали Германии: ' + today2.toLocaleString('de-DE');
+
+    let out3 = document.getElementById('current-date3');
+    let today3 = new Date();
+    out3.innerHTML = 'Дата и время для локали Испании (Аргентина): ' +  today3.toLocaleString('es-AR');
+
+    let out4 = document.getElementById('current-date4');
+    let today4 = new Date();
+    out4.innerHTML = 'Дата и время для локали Таиланда: ' +   today4.toLocaleString('th-TH');
+
+    let out5 = document.getElementById('current-date5');
+    let today5 = new Date();
+    out5.innerHTML = 'Дата и время для локали Албании: ' +   today5.toLocaleString('sq');
+}
